2146675d3b7b7d535f87e9598de231088cb696d5
Kevin Camellini <kevincamellini@gmail.com>
2018-09-20 02:25:16 -0400

    database environment setup

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Express setup
+var app = express();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+//  DB connection
+var connString = process.env.DATABASE_URL || 'postgres://hizxyalrympljm:3f4cd73544ce42e3aade5131e9d72f3d4032b8e69ac8fc37d8b8186cf3de4a3d@ec2-54-83-27-165.compute-1.amazonaws.com:5432/d6a0flgsl8bp0c';
+const { Pool } = require('pg');
+const pool = new Pool({
+  	connectionString: connString,
+  	ssl: true
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET db page */
+router.get('/db', async (req, res) => {
+	try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows : null};
+        res.render('db', results );
+        client.release();
+	} catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+	}
+})
 
 module.exports = router;
