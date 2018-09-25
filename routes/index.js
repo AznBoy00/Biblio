66b2a2db1d11d01e08125722f995a2a4a69977ab
AznBoy00 <Lin_Kevin_1995@hotmail.com>
2018-09-25 13:51:34 -0400

    Moved YML file, Uniformized some lines, Re-indented HTML tags on EJS files.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-//  DB connection
+// DB connection
 var connString = process.env.DATABASE_URL || 'postgres://hizxyalrympljm:3f4cd73544ce42e3aade5131e9d72f3d4032b8e69ac8fc37d8b8186cf3de4a3d@ec2-54-83-27-165.compute-1.amazonaws.com:5432/d6a0flgsl8bp0c';
 const { Pool } = require('pg');
 const pool = new Pool({
@@ -9,7 +9,7 @@ const pool = new Pool({
   	ssl: true
 });
 
-/* GET home page. */
+// GET homepage
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
@@ -22,17 +22,17 @@ router.get('/signup', function(req, res, next) {
         res.render('components/login/signup.ejs', { title: 'Express' });
 });
 
-/* GET db page */
+// GET db page
 router.get('/db', async (req, res) => {
 	try {
-        const client = await pool.connect()
-        const result = await client.query('SELECT * FROM test_table');
-        const results = { 'results': (result) ? result.rows : null};
-        res.render('db', results );
-        client.release();
+                const client = await pool.connect()
+                const result = await client.query('SELECT * FROM test_table');
+                const results = { 'results': (result) ? result.rows : null};
+                res.render('db', results );
+                client.release();
 	} catch (err) {
-        console.error(err);
-        res.send("Error " + err);
+                console.error(err);
+                res.send("Error " + err);
 	}
 })
 
