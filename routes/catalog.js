ded74fcd2a460ef89cb2fdf601ea5c7845d3d3b0
Abdulla Alhaj Zin <abdullah.hzen@gmail.com>
2018-10-11 17:29:33 -0400

    Adding an empty architecture to match the class diagram design

---
+++ routes/catalog.js
@@ -0,0 +1,8 @@
+//to access the routes in this class, include /catalog/{ROUTE_FROM_THIS_FILE} in your URL
+var express = require('express');
+var router = express.Router();
+
+
+
+//keep the next line at the end of this script
+module.exports = router;
