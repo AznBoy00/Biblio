376c290bc279f885f59e11d4a4559fe5dd89706f
AznBoy00 <Lin_Kevin_1995@hotmail.com>
2018-10-12 16:37:47 -0400

    testing sql

--- models/catalog.js
+++ models/catalog.js
@@ -19,18 +19,8 @@ module.exports.insertNewBook = async function(newbook) {
     try {
         const client = await pool.connect();
 
-        const newitem = await client.query(
-            "INSERT INTO Items (discriminator) VALUE ('Book')",
-            function(err, result) {
-                if (err)
-                    console.log(err);
-                else
-                    console.log(newitem);
-            }
-        );
-
         const itemid = await client.query(
-            "SELECT MAX(item_id) FROM Items",
+            "SELECT MAX(item_id) FROM Items;",
             function(err, result) {
                 if (err)
                     console.log(err);
@@ -39,27 +29,39 @@ module.exports.insertNewBook = async function(newbook) {
             }
         );
 
-        const result = await client.query(
-            "INSERT INTO Books (book_id, quantity, title, author, format, pages, publisher, language, isbn10, isbn13) VALUES ('"
-            + itemid + "','"
-            + newbook.quantity + "','"
-            + newbook.title + "','"
-            + newbook.author+ "','"
-            + newbook.format + "','"
-            + newbook.pages + "','"
-            + newbook.publisher + "','"
-            + newbook.language + "','"
-            + newbook.isbn10 + "','"
-            + newbook.isbn13 + "')" ,function(err, result){
-            if (err) {
-                console.log(err);
-            }
-            else {
-                console.log(result);
-            }
-        });
+        itemid = itemid + 1;
 
-    } catch (err) {
-        console.error(err);
-    }
+        const newitem = await client.query(
+            "INSERT INTO Items (item_id, discriminator) VALUES (" + itemid[0] + ", 'Book')",
+            function(err, result) {
+                if (err)
+                    console.log(err);
+                else
+                    console.log(newitem);
+            }
+        );
+
+        // const result = await client.query(
+        //     "INSERT INTO Books (book_id, quantity, title, author, format, pages, publisher, language, isbn10, isbn13) VALUES ("
+        //                 + itemid + ","
+        //                 + newbook.quantity + ",'"
+        //                 + newbook.title + "','"
+        //                 + newbook.author+ "','"
+        //                 + newbook.format + "',"
+        //                 + newbook.pages + ",'"
+        //                 + newbook.publisher + "','"
+        //                 + newbook.language + "',"
+        //                 + newbook.isbn10 + ","
+        //                 + newbook.isbn13 + ")" ,            
+        //     function(err, result){
+        //         if (err) {
+        //             console.log(err);
+        //         }
+        //         else {
+        //             console.log(result);
+        //         }
+        //     });
+        } catch (err) {
+            console.error(err);
+        }
 };
