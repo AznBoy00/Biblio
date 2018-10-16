b5f1942cd8c095f19c334832cfcdae4fad141b39
Kevin Camellini <kevincamellini@gmail.com>
2018-10-16 03:05:37 -0400

    db connecttion abstracted #42

--- models/users.js
+++ models/users.js
@@ -1,10 +1,5 @@
-// DB connection
-var connString = process.env.DATABASE_URL || 'postgres://hizxyalrympljm:3f4cd73544ce42e3aade5131e9d72f3d4032b8e69ac8fc37d8b8186cf3de4a3d@ec2-54-83-27-165.compute-1.amazonaws.com:5432/d6a0flgsl8bp0c';
-const { Pool } = require('pg');
-const pool = new Pool({
-    connectionString: connString,
-    ssl: true
-});
+// DB Connection
+const pool = require('../db');
 const bcrypt = require('bcrypt-nodejs');
 
 module.exports.insertNewUser = async function(newUser) {
--- routes/catalog.js
+++ routes/catalog.js
@@ -4,13 +4,8 @@ var router = express.Router();
 var expressValidator = require('express-validator');
 router.use(expressValidator());
 var catalog = require('../models/catalog');
-// DB connection
-var connString = 'postgres://hizxyalrympljm:3f4cd73544ce42e3aade5131e9d72f3d4032b8e69ac8fc37d8b8186cf3de4a3d@ec2-54-83-27-165.compute-1.amazonaws.com:5432/d6a0flgsl8bp0c' || process.env.DATABASE_URL;
-const { Pool } = require('pg');
-const pool = new Pool({
-  	connectionString: connString,
-  	ssl: true
-});
+// DB Connection
+const pool = require('../db');
 
 
 // ====================================== //
