0c8711354e4bc4b548961ff46ddac8f4f90e1616
Kenza <kenza.boulisfane@gmail.com>
2018-10-16 23:53:20 -0400

    added insertNewBook and insertNewMagazine methods

--- models/catalog.js
+++ models/catalog.js
@@ -1,6 +1,3 @@
-var item = require('./models/item');
-
-
 // DB connection
 var connString = process.env.DATABASE_URL || 'postgres://hizxyalrympljm:3f4cd73544ce42e3aade5131e9d72f3d4032b8e69ac8fc37d8b8186cf3de4a3d@ec2-54-83-27-165.compute-1.amazonaws.com:5432/d6a0flgsl8bp0c';
 const { Pool } = require('pg');
@@ -9,8 +6,60 @@ const pool = new Pool({
     ssl: true
 });
 
-//list to be filled with item objects from item.js in models
-var itemsList = [];
+//Insert new book into db
+module.exports.insertNewBook = async function(newBook) {
+    try {
+        const client = await pool.connect();
+            const result = await admin.query("INSERT INTO Book (title, author, format, pages, publisher, language, isbn10, isbn13, quantity) VALUES ('"
+                + newBook.title + "','"
+                + newBook.author + "','"
+                + newBook.format+ "','"
+                + newBook.pages + "','"
+                + newBook.publisher + "','"
+                + newBook.language + "','"
+                + newBook.isbn10 + "','"
+                + newBook.isbn13 + "','"
+                + newBook.quantity + "')" ,function(err, result){
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log(result);
+                }
+            });
 
-//below is an example of using the constructor of the object
-// item.constructor(item_id, discriminator, properties (as an object));
+    } catch (err) {
+        console.error(err);
+        res.send(err);
+    }
+};
+
+//Insert new magazine into db
+module.exports.insertNewMagazine = async function(newMagazine) {
+    try {
+        const client = await pool.connect();
+            const result = await admin.query("INSERT INTO Magazine (title, artist, label, releaseDate, language, quantity, asin) VALUES ('"
+                + newMagazine.title + "','"
+                + newMagazine.artist + "','"
+                + newMagazine.label+ "','"
+                + newMagazine.releaseDate + "','"
+                + newMagazine.publisher + "','"
+                + newMagazine.language + "','"
+                + newMagazine.quantity + "','"
+                + newMagazine.asin + "')" ,function(err, result){
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log(result);
+                }
+            });
+
+    } catch (err) {
+        console.error(err);
+        res.send(err);
+    }
+};
+
+
+
