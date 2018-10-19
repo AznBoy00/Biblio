18db5f03e26a7379a6c282efdece5469a43b0ee5
Nour El Natour <nour_elnatour@hotmail.com>
2018-10-19 17:43:04 -0400

    Adding session to check logged-in in index.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 // Config Variables
+var session = require('express-session');
 var express = require('express');
 var router = express.Router();
+router.use(session({
+    secret : '2C44-4D44-WppQ38S',
+    resave : true,
+    saveUninitialized : true
+}));
 
 // GET homepage
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  var is_logged = false;
+  if (typeof req.session !== 'undefined' && typeof req.session.logged !== 'undefined') {
+    is_logged = req.session.logged;
+    res.render('index', { title: 'Home', is_logged: is_logged });
+  } else {
+      res.render('index', { title: 'Home' });
+  }
 });
 
 module.exports = router;
