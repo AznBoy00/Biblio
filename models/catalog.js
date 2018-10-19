3f6c06dce70f1da71ae7387eefc302c0dcc24a1c
Kenza <kenza.boulisfane@gmail.com>
2018-10-18 23:35:09 -0400

    Update insert items into db (still not done and not tested)

--- models/catalog.js
+++ models/catalog.js
@@ -6,121 +6,80 @@ const pool = new Pool({
     ssl: true
 });
 
+//insert new item into db
 
-//Create new book: Post request
-
-var item = require('../models/item');
-
-// Create
-
-
-//insert new book
-
-module.exports.insertNewItem = async function(newItem) {
+router.post('/createItem/:item_id', async (req, res) => {
     try {
         const client = await pool.connect();
+        const result = await client.query("INSERT INTO Items (discriminator) VALUES (discriminator);
 
-        let itemid = await client.query(
-            "SELECT MAX(item_id) FROM Items;",
-            function(err, result) {
-                if (err)
-                    console.log(err);
-                else
-                    console.log(itemid);
-            }
-        );
-
-        itemid = itemid + 1;
-
-        const newItem = await client.query(
-            "INSERT INTO Items (item_id, discriminator) VALUES (" + itemid[0] + ", 'Items')",
-            function(err, result) {
-                if (err)
-                    console.log(err);
-                else
-                    console.log('1 record inserted');
-            }
-        );
-
-        const result = await client.query("INSERT INTO Book (discriminator) VALUES ("
-                         + itemid + ","
-                       + newbook.quantity + ",'"
-                       + newbook.title + "','"
-                        + newbook.author+ "','"
-                + newbook.format + "',"
-                         + newbook.pages + ",'"
-                         + newbook.publisher + "','"
-                         + newbook.language + "',"
-                         + newbook.isbn10 + ","
-                         + newbook.isbn13 + ")" ,
-             function(err, result){
-                 if (err) {
-                     console.log(err);
-                 }
-                 else {
-                     console.log(result);
-                 }
-             });
+            var itemList[] = ['title', 'author', 'format', 'quantity', 'pages', 'publisher'];
+        const results = { 'results': (result) ? result.rows : null};
+        res.render('catalog/createItem', {results, title: 'Catalog'});
+        client.release();
     } catch (err) {
         console.error(err);
+        res.render('error', { error: err });
     }
-};
+    switch(discriminator){
+        case "Book":
+            module.exports.insertNewBook = async function(newBook) {
+                try {
+                    const client = await pool.connect();
+                    const result = await client.query("INSERT INTO Book (title, author, format, pages, publisher, language, isbn10, isbn13, quantity) VALUES ('"
+                        + newBook.title + "','"
+                        + newBook.author + "','"
+                        + newBook.format+ "','"
+                        + newBook.pages + "','"
+                        + newBook.publisher + "','"
+                        + newBook.language + "','"
+                        + newBook.isbn10 + "','"
+                        + newBook.isbn13 + "','"
+                        + newBook.quantity + "')" ,function(err, result){
+                        if (err) {
+                            console.log(err);
+                        }
+                        else {
+                            console.log(result);
+                        }
+                    });
 
-//Insert new book into db (without using Item table)
-module.exports.insertNewBook = async function(newBook) {
-    try {
-        const client = await pool.connect();
-            const result = await client.query("INSERT INTO Book (title, author, format, pages, publisher, language, isbn10, isbn13, quantity) VALUES ('"
-                + newBook.title + "','"
-                + newBook.author + "','"
-                + newBook.format+ "','"
-                + newBook.pages + "','"
-                + newBook.publisher + "','"
-                + newBook.language + "','"
-                + newBook.isbn10 + "','"
-                + newBook.isbn13 + "','"
-                + newBook.quantity + "')" ,function(err, result){
-                if (err) {
-                    console.log(err);
+                } catch (err) {
+                    console.error(err);
+                    res.send(err);
                 }
-                else {
-                    console.log(result);
+            };
+        case "Magazine":
+            module.exports.insertNewMagazine = async function(newMagazine){
+                try {
+                    const client = await pool.connect();
+                    const result = await client.query("INSERT INTO Magazine (title, author, format, pages, publisher, language, isbn10, isbn13, quantity) VALUES ('"
+                        + newMagazine.title + "','"
+                        + newMagazine.author + "','"
+                        + newMagazine.format+ "','"
+                        + newMagazine.pages + "','"
+                        + newMagazine.publisher + "','"
+                        + newMagazine.language + "','"
+                        + newMagazine.isbn10 + "','"
+                        + newMagazine.isbn13 + "','"
+                        + newMagazine.quantity + "')" ,function(err, result){
+                        if (err) {
+                            console.log(err);
+                        }
+                        else {
+                            console.log(result);
+                        }
+                    });
+
+                } catch (err) {
+                    console.error(err);
+                    res.send(err);
                 }
-            });
+            };
+        case "Music":
 
-    } catch (err) {
-        console.error(err);
-        res.send(err);
-    }
-};
-
-//Insert new magazine into db (without using Item table)
-module.exports.insertNewMagazine = async function(newMagazine) {
-    try {
-        const client = await pool.connect();
-            const result = await client.query("INSERT INTO Magazine (title, artist, label, releaseDate, language, quantity, asin) VALUES ('"
-                + newMagazine.title + "','"
-                + newMagazine.artist + "','"
-                + newMagazine.label+ "','"
-                + newMagazine.releaseDate + "','"
-                + newMagazine.publisher + "','"
-                + newMagazine.language + "','"
-                + newMagazine.quantity + "','"
-                + newMagazine.asin + "')" ,function(err, result){
-                if (err) {
-                    console.log(err);
-                }
-                else {
-                    console.log(result);
-                }
-            });
-
-    } catch (err) {
-        console.error(err);
-        res.send(err);
-    }
-};
-
+    };
+});
 
 
 
