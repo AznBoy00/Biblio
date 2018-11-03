39cbdde4d95289a4165c5de640b75c22a4a94483
Kevin Camellini <kevincamellini@gmail.com>
2018-11-03 02:27:29 -0400

    refactoring to in routes/catalog.js
    removed redundant switch cases
    removed redundant/duplicated routes
    updated view/catlog/creatitems and viewItems to refelct these changes
    #68

--- routes/catalog.js
+++ routes/catalog.js
@@ -44,28 +44,16 @@ router.get('/viewItem/:item_id', async (req, res) => {
 // ====================================== //
 // == GET Requests for Creating Items === //
 // ====================================== //
-//
+
 // is_logged is passed to check the session in the front-end
-//
-// Create a new item page
+// Page to select which item ti unsert. Upon selecting
+// the specific item create/discriminator is rendered
 router.get('/createitems', function (req, res, next) {
     res.render('catalog/createitem', { title: 'Create Item', is_logged: req.session.logged, is_admin: req.session.is_admin});
 });
 // Create a new book
-router.get('/createitems/createBook', function (req, res, next) {
-    res.render('catalog/createBook', { title: 'Create Item', is_logged: req.session.logged, is_admin: req.session.is_admin});
-});
-// Create a new magazine
-router.get('/createitems/createMagazine', function (req, res, next) {
-    res.render('catalog/createMagazine', { title: 'Create Item', is_logged: req.session.logged, is_admin: req.session.is_admin});
-});
-// Create a music
-router.get('/createitems/createMusic', function (req, res, next) {
-    res.render('catalog/createMusic', { title: 'Create Item', is_logged: req.session.logged, is_admin: req.session.is_admin});
-});
-// Create a new movie
-router.get('/createitems/createMovie', function (req, res, next) {
-    res.render('catalog/createMovie', { title: 'Create Item', is_logged: req.session.logged, is_admin: req.session.is_admin});
+router.get('/createitems/:discriminator', function (req, res, next) {
+    res.render('catalog/create'+req.params.discriminator, { title: 'Create Item', is_logged: req.session.logged, is_admin: req.session.is_admin});
 });
 
 
@@ -75,10 +63,8 @@ router.get('/createitems/createMovie', function (req, res, next) {
 
 router.post('/createitems/create/:discriminator', async (req, res) => {
     try {
-        let result;
-        let newItem;
-        newItem = await catalog.getNewItemForInsert(req.params.discriminator, req);
-        result = await catalog.insertNewItem(newItem, req.params.discriminator);
+        let newItem = await catalog.getNewItemForInsert(req.params.discriminator, req);
+        let result = await catalog.insertNewItem(newItem, req.params.discriminator);
         res.redirect('/catalog');
     } catch (err) {
         console.error(err);
@@ -94,25 +80,7 @@ router.get('/updateitem/:item_id', async (req, res) => {
     try {
         let results = await catalog.getItem(req.params.item_id);
         let discriminator = await catalog.getDiscriminator(req.params.item_id);
-        console.log(discriminator);
-        console.log(results.results[0].item_id);
-        switch (discriminator) {
-            case "Book":
-                res.render('catalog/updateBook', { results, title: 'Catalog', is_logged: req.session.logged, is_admin: req.session.is_admin});
-                break;
-            case "Magazine":
-                res.render('catalog/updateMagazine', { results, title: 'Catalog', is_logged: req.session.logged, is_admin: req.session.is_admin });
-                break;
-            case "Movie":
-                res.render('catalog/updateMovie', { results, title: 'Catalog', is_logged: req.session.logged, is_admin: req.session.is_admin });
-                break;
-            case "Music":
-                res.render('catalog/updateMusic', { results, title: 'Catalog', is_logged: req.session.logged, is_admin: req.session.is_admin });
-                break;
-            default:
-                result = null;
-                break;
-        }
+        res.render('catalog/update'+discriminator, { results, title: 'Catalog', is_logged: req.session.logged, is_admin: req.session.is_admin});
     } catch (err) {
         console.error(err);
         res.render('error', { error: err });
@@ -143,12 +111,12 @@ router.post('/updateitem/:item_id/modify', async (req, res) => {
 // ====================================== //
 
 // Books
-router.get('/deletebook/:item_id', async(req, res) => {
+router.get('/deleteitem/:item_id', async(req, res) => {
 
     try {
         //front-end will confirm if the item is to be deleted or not
         const client = await pool.connect();
-        const resultDeleteBook = await client.query('DELETE FROM Items WHERE item_id = ($1)', [req.params.item_id]);
+        const result = await client.query('DELETE FROM Items WHERE item_id = ($1)', [req.params.item_id]);
         res.redirect('/catalog'); //refresh the page with the new changes 
         client.release();
     } catch (err) {
@@ -159,49 +127,49 @@ router.get('/deletebook/:item_id', async(req, res) => {
 
 
 //Magazine
-router.get('/deletemagazine/:item_id', async(req, res) => {
+// router.get('/deletemagazine/:item_id', async(req, res) => {
 
-    try {
-        //front-end will confirm if the item is to be deleted or not
-        const client = await pool.connect();
-        const resultMagazine = await client.query('DELETE FROM Items WHERE item_id = ($1)', [req.params.item_id]);
-        res.redirect('/catalog');//refresh the page with the new changes 
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
-});
+//     try {
+//         //front-end will confirm if the item is to be deleted or not
+//         const client = await pool.connect();
+//         const resultMagazine = await client.query('DELETE FROM Items WHERE item_id = ($1)', [req.params.item_id]);
+//         res.redirect('/catalog');//refresh the page with the new changes 
+//         client.release();
+//     } catch (err) {
+//         console.error(err);
+//         res.send("Error " + err);
+//     }
+// });
 
-//Movies
-router.get('/deletemovie/:item_id', async(req, res) => {
+// //Movies
+// router.get('/deletemovie/:item_id', async(req, res) => {
 
-    try {
-        //front-end will confirm if the item is to be deleted or not
-        const client = await pool.connect();
-        const resultDeleteMovie = await client.query('DELETE FROM Items WHERE item_id = ($1)', [req.params.item_id]);
-        res.redirect('/catalog');//refresh the page with the new changes 
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
-});
+//     try {
+//         //front-end will confirm if the item is to be deleted or not
+//         const client = await pool.connect();
+//         const resultDeleteMovie = await client.query('DELETE FROM Items WHERE item_id = ($1)', [req.params.item_id]);
+//         res.redirect('/catalog');//refresh the page with the new changes 
+//         client.release();
+//     } catch (err) {
+//         console.error(err);
+//         res.send("Error " + err);
+//     }
+// });
 
-//Music
-router.get('/deletemusic/:item_id', async(req, res) => {
+// //Music
+// router.get('/deletemusic/:item_id', async(req, res) => {
 
-    try {
-        //front-end will confirm if the item is to be deleted or not
-        const client = await pool.connect();
-        const resultDeleteMusic = await client.query('DELETE FROM Items WHERE item_id = ($1)', [req.params.item_id]);
-        res.redirect('/catalog');//refresh the page with the new changes 
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
-});
+//     try {
+//         //front-end will confirm if the item is to be deleted or not
+//         const client = await pool.connect();
+//         const resultDeleteMusic = await client.query('DELETE FROM Items WHERE item_id = ($1)', [req.params.item_id]);
+//         res.redirect('/catalog');//refresh the page with the new changes 
+//         client.release();
+//     } catch (err) {
+//         console.error(err);
+//         res.send("Error " + err);
+//     }
+// });
 
 
 
