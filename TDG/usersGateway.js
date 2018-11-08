64b759ffa5d6c2660e277a2f4e3ca168f56b5cc5
KH <kh_group@outlook.com>
2018-11-07 23:46:23 -0500

    Add all except one module to the user TDG
    tested all

--- TDG/usersGateway.js
+++ TDG/usersGateway.js
@@ -20,7 +20,6 @@ module.exports.createNewUser = async function(newUser){
         }
     });
     client.release();
-
     return result;
 }
 
@@ -31,20 +30,47 @@ module.exports.getUserByEmail = async function(email){
 
 //displayAllUsers Model
 module.exports.getAllUsers = async function(){
-
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM users ORDER BY user_id ASC');
+    const results = { 'results': (result) ? result.rows : null};
+    client.release();
+    return results;
 }
 
 //toggleAdminStatus Model
-module.exports.changeAdminStatus = function(userid, is_admin){
-
+module.exports.changeAdminStatus = async function(userid, is_admin){
+    let result;
+    var toggle;
+    const client = await pool.connect();
+    if (is_admin == false)
+        toggle = 't';
+    else
+        toggle = 'f';
+    result = await client.query("UPDATE users SET is_admin = '" + toggle + "' WHERE user_id = ($1)", [userid]);
 }
 
 //getUserInfo Model
-module.exports.getUserInfo = function(email){
-
+module.exports.getUserInfo = async function(email){
+    const client = await pool.connect()
+    let result;
+    result = await client.query("SELECT * FROM Users WHERE email = ($1)", [email]);
+    const results = { 'results': (result) ? result.rows : null};
+    client.release();
+    return results;
 }
 
 //updateUserInfo Model
-module.exports.updateUserInfo = function(newUserInfo, email){
+module.exports.updateUserInfo = async function(newUserInfo, email){
+    const client = await pool.connect();
+        let result;
 
+            result = await client.query(
+                    "UPDATE users SET " +
+                    "f_name = '"+ newUserInfo.f_name + "', " +
+                    "l_name = '" + newUserInfo.l_name + "', " +
+                    "phone = '" + newUserInfo.phone + "', " +
+                    "password = '" + newUserInfo.password + "' " +
+                    "WHERE email = ($1);", [email]
+                );
+        client.release();
 }
--- models/users.js
+++ models/users.js
@@ -1,5 +1,7 @@
 var tdg = require('../TDG/usersGateway');
 const bcrypt = require('bcrypt-nodejs');
+const pool = require('../db');
+
 
 //registers a new user to the DB
 module.exports.insertNewUser = async function(newUser) {
@@ -16,8 +18,9 @@ async function findUserByEmail(email){
     try {
         const client = await pool.connect();
         const result = await client.query('SELECT * FROM Users WHERE Users.email = \'' + email + '\'');
-        client.release();
+        client.release();    
         return await result;
+        
     } catch (err) {
         console.error(err);
         res.send("Error " + err);
@@ -26,7 +29,7 @@ async function findUserByEmail(email){
 
 //check if a user exists by email
 module.exports.userExists = async function (email) {
-    var result = await findUserByEmail(email);
+    var result = await findUserByEmail(email); 
     if (result.rows.length == 1){
        return true;
     } else {
@@ -64,11 +67,7 @@ module.exports.findUserByEmail = async function findUserByEmail(email){
 //display all users
 module.exports.displayAllUsers = async function() {
     try {
-        const client = await pool.connect()
-        const result = await client.query('SELECT * FROM users ORDER BY user_id ASC');
-        const results = { 'results': (result) ? result.rows : null};
-        client.release();
-        return await results;
+        return await tdg.getAllUsers();
     } catch (err) {
         console.error(err);
 	}
@@ -82,14 +81,7 @@ async function checkIsAdmin(userid) {
 //toggles the promotion and demotion to admin for a user
 module.exports.toggleAdminStatus = async function(userid, is_admin) {
     try {
-        let result;
-        var toggle;
-        const client = await pool.connect();
-        if (is_admin == false)
-            toggle = 't';
-        else
-            toggle = 'f';
-        result = await client.query("UPDATE users SET is_admin = '" + toggle + "' WHERE user_id = ($1)", [userid]);
+        return await tdg.changeAdminStatus(userid, is_admin);
     } catch (err) {
         console.error(err);
     }
@@ -97,12 +89,7 @@ module.exports.toggleAdminStatus = async function(userid, is_admin) {
 //getter for user profile information
 module.exports.getUserInfo = async function(email) {
     try {
-        const client = await pool.connect()
-        let result;
-        result = await client.query("SELECT * FROM Users WHERE email = ($1)", [email]);
-        const results = { 'results': (result) ? result.rows : null};
-        client.release();
-        return await results;
+        return await tdg.getUserInfo(email);
     } catch (err) {
         console.error(err);
 	}
@@ -139,18 +126,8 @@ module.exports.getNewUserInfo = async function(email, req) {
 // update user information to database
 module.exports.updateUserInfo = async function(newUserInfo, email) {
     try {
-        const client = await pool.connect();
-        let result;
+    return await tdg.updateUserInfo(newUserInfo, email);
 
-            result = await client.query(
-                    "UPDATE users SET " +
-                    "f_name = '"+ newUserInfo.f_name + "', " +
-                    "l_name = '" + newUserInfo.l_name + "', " +
-                    "phone = '" + newUserInfo.phone + "', " +
-                    "password = '" + newUserInfo.password + "' " +
-                    "WHERE email = ($1);", [email]
-                );
-        client.release();
     } catch (err) {
         console.error(err);
     }
